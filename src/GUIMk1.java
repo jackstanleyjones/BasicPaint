30a208b3a7bfe28c5bcf4c78ac68f0ec284f0fc3
corey <corals.corey@gmail.com>
2019-05-15 15:22:09 +1000

    gui mark 1 runnable with a draggable square of set dimensions that can be created on click

--- src/GUIMk1.java
+++ src/GUIMk1.java
@@ -1,42 +1,76 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
 public class GUIMk1 {
     private JPanel panel1;
-
-    abstract class Mycanvas extends JPanel implements MouseListener {
-        Image img;      // Contains the image to draw on MyCanvas
-
-        public Mycanvas() {
-            // Initialize img here.
-            this.addMouseListener(this);
-        }
-
-        public void paintComponent(Graphics g) {
-            // Draws the image to the canvas
-            g.drawImage(img, 0, 0, null);
-        }
-
-        public void mouseClicked(MouseEvent e) {
-            int x = e.getX();
-            int y = e.getY();
-
-            Graphics g = img.getGraphics();
-            g.fillOval(x, y, 3, 3);
-            g.dispose();
-        }
-
-
+    public static void main(String[] args){
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                MakeGUI();
+            }
+        });
     }
 
-    public static void main(String[] args){
+    private static void MakeGUI(){
         JFrame frame = new JFrame("GUIMk1");
-        frame.setContentPane(new GUIMk1().panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(new MyPanel());
         frame.pack();
         frame.setVisible(true);
+    }
+
+
+}
+
+class MyPanel extends JPanel{
+    private int squareX = 50;
+    private int squareY = 50;
+    private int squareW = 20;
+    private int squareH = 20;
+
+    public MyPanel() {
+
+        setBorder(BorderFactory.createLineBorder(Color.black));
+
+        addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+                moveSquare(e.getX(),e.getY());
+            }
+        });
+
+        addMouseMotionListener(new MouseAdapter() {
+            public void mouseDragged(MouseEvent e) {
+                moveSquare(e.getX(),e.getY());
+            }
+        });
 
     }
+
+    private void moveSquare(int x, int y) {
+        int OFFSET = 1;
+        if ((squareX!=x) || (squareY!=y)) {
+            repaint(squareX,squareY,squareW+OFFSET,squareH+OFFSET);
+            squareX=x;
+            squareY=y;
+            repaint(squareX,squareY,squareW+OFFSET,squareH+OFFSET);
+        }
+    }
+
+    public Dimension getPreferredSize() {
+        return new Dimension(250,200);
+    }
+
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(Color.RED);
+        g.fillRect(squareX,squareY,squareW,squareH);
+        g.setColor(Color.BLACK);
+        g.drawRect(squareX,squareY,squareW,squareH);
+    }
 }
+
+
+
+
