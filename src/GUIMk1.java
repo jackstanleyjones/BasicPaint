4aa1e351a9f91b6dee4cde0f02c55b53042ac7d6
jackstanleyjones <stanleyjonesjack@gmail.com>
2019-05-18 10:09:18 +1000

    basic toolbar

--- src/GUIMk1.java
+++ src/GUIMk1.java
@@ -16,6 +16,8 @@ public class GUIMk1 {
     private static void MakeGUI(){
         JFrame frame = new JFrame("GUIMk1");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(new Toolbar());
+
         frame.add(new MyPanel());
         frame.pack();
         frame.setVisible(true);
@@ -24,25 +26,25 @@ public class GUIMk1 {
 
 }
 
-class MyPanel extends JPanel{
+class MyPanel extends JPanel {
     private int squareX = 50;
     private int squareY = 50;
     private int squareW = 20;
     private int squareH = 20;
 
-    public MyPanel() {
+    MyPanel() {
 
         setBorder(BorderFactory.createLineBorder(Color.black));
 
         addMouseListener(new MouseAdapter() {
             public void mousePressed(MouseEvent e) {
-                moveSquare(e.getX(),e.getY());
+                moveSquare(e.getX(), e.getY());
             }
         });
 
         addMouseMotionListener(new MouseAdapter() {
             public void mouseDragged(MouseEvent e) {
-                moveSquare(e.getX(),e.getY());
+                moveSquare(e.getX(), e.getY());
             }
         });
 
@@ -50,26 +52,49 @@ class MyPanel extends JPanel{
 
     private void moveSquare(int x, int y) {
         int OFFSET = 1;
-        if ((squareX!=x) || (squareY!=y)) {
-            repaint(squareX,squareY,squareW+OFFSET,squareH+OFFSET);
-            squareX=x;
-            squareY=y;
-            repaint(squareX,squareY,squareW+OFFSET,squareH+OFFSET);
+        if ((squareX != x) || (squareY != y)) {
+            repaint(squareX, squareY, squareW + OFFSET, squareH + OFFSET);
+            squareX = x;
+            squareY = y;
+            repaint(squareX, squareY, squareW + OFFSET, squareH + OFFSET);
         }
     }
 
     public Dimension getPreferredSize() {
-        return new Dimension(250,200);
+        return new Dimension(250, 250);
     }
 
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.setColor(Color.RED);
-        g.fillRect(squareX,squareY,squareW,squareH);
+        g.fillRect(squareX, squareY, squareW, squareH);
         g.setColor(Color.BLACK);
-        g.drawRect(squareX,squareY,squareW,squareH);
+        g.drawRect(squareX, squareY, squareW, squareH);
+    }
+
+}
+
+class Toolbar extends JPanel {
+
+    Toolbar(){
+
+        setBorder(BorderFactory.createLineBorder(Color.black));
+        setSize(250,20);
+
+
+        JToolBar toolBar = new JToolBar("Still draggable");
+        JButton plot = new JButton("plot");
+        toolBar.add(plot, BorderLayout.NORTH);
+
+        //Create the text area used for output.  Request
+        //enough space for 5 rows and 30 columns.
+        //Lay out the main panel.
+        //setPreferredSize(new Dimension(450, 130));
     }
 }
+
+
+
 
 
 
