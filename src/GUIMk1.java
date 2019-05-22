d6d6fb2926b5e48f1183f3a95a3f5d874eb837fc
jackstanleyjones <stanleyjonesjack@gmail.com>
2019-05-22 16:12:58 +1000

    Border color working

--- src/GUIMk1.java
+++ src/GUIMk1.java
@@ -1,10 +1,11 @@
 import javax.swing.*;
 import javax.swing.event.MouseInputAdapter;
-import javax.tools.Tool;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.util.List;
 import java.awt.event.*;
-import java.awt.geom.Ellipse2D;
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 public class GUIMk1 {
@@ -40,8 +41,9 @@ public class GUIMk1 {
     }
 
     static class ToolSelect extends JPanel implements ActionListener{
-        public static String toolSelction = "plot";
+        public static String toolSelection = "plot";
         public ButtonGroup toolGroup = new ButtonGroup();
+        public static Color borderColor = Color.BLACK;
 
         private DrawingArea drawingArea;
 
@@ -52,7 +54,15 @@ public class GUIMk1 {
             toolbar.add(makeButton("line"));
             toolbar.add(makeButton("rectangle"));
             toolbar.add(makeButton("ellipse"));
+            ColorChooserButton colorChooser = new ColorChooserButton(Color.WHITE);
+            colorChooser.addColorChangedListener(new ColorChooserButton.ColorChangedListener() {
+                @Override
+                public void colorChanged(Color newColor) {
+                    setColor(newColor);
+                }
+            });
             toolbar.add(makeButton("polygon"));
+            toolbar.add(colorChooser);
             add(toolbar);
 
         }
@@ -69,12 +79,20 @@ public class GUIMk1 {
          }
 
          private void setToolSelction(String tool){
-            this.toolSelction = tool;
+            this.toolSelection = tool;
+        }
+
+        private void setColor(Color c){
+            this.borderColor = c;
+        }
+
+        public static Color GetColor(){
+            return borderColor;
         }
 
 
         public static String GetTool(){
-            return toolSelction;
+            return toolSelection;
         }
 
 
@@ -87,8 +105,72 @@ public class GUIMk1 {
         }
     }
 
-    /*
-    static class ColorSelect extends JPanel implements ActionListener{
+
+    public static class ColorChooserButton extends JButton {
+
+        private Color current;
+
+        public ColorChooserButton(Color c) {
+            setSelectedColor(c);
+            addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent arg0) {
+                    Color newColor = JColorChooser.showDialog(null, "Choose a color", current);
+                    setSelectedColor(newColor);
+                    System.out.print(newColor);
+                }
+            });
+        }
+
+        public Color getSelectedColor() {
+            return current;
+        }
+
+        public void setSelectedColor(Color newColor) {
+            setSelectedColor(newColor, true);
+        }
+
+        public void setSelectedColor(Color newColor, boolean notify) {
+
+            if (newColor == null) return;
+
+            current = newColor;
+            setIcon(createIcon(current, 16, 16));
+            repaint();
+
+            if (notify) {
+                // Notify everybody that may be interested.
+                for (ColorChangedListener l : listeners) {
+                    l.colorChanged(newColor);
+                }
+            }
+        }
+
+        public interface ColorChangedListener {
+            public void colorChanged(Color newColor);
+        }
+
+        private List<ColorChangedListener> listeners = new ArrayList<ColorChangedListener>();
+
+        public void addColorChangedListener(ColorChangedListener toAdd) {
+            listeners.add(toAdd);
+        }
+
+        public ImageIcon createIcon(Color main, int width, int height) {
+            BufferedImage image = new BufferedImage(width, height, java.awt.image.BufferedImage.TYPE_INT_RGB);
+            Graphics2D graphics = image.createGraphics();
+            graphics.setColor(main);
+            graphics.fillRect(0, 0, width, height);
+            graphics.setXORMode(Color.DARK_GRAY);
+            graphics.drawRect(0, 0, width-1, height-1);
+            image.flush();
+            ImageIcon icon = new ImageIcon(image);
+            return icon;
+        }
+    }
+
+    /*static class ColourSelect extends JPanel implements ActionListener{
+        public Color colour = Color.BLACK;
         public ButtonGroup toolGroup = new ButtonGroup();
 
         private DrawingArea drawingArea;
@@ -97,19 +179,25 @@ public class GUIMk1 {
             this.drawingArea = drawingArea;
             JToolBar toolbar  = new JToolBar(null, JToolBar.VERTICAL);
             toolbar.add(makeButton("blue"));
-            toolbar.add(makeButton("line"));
-            toolbar.add(makeButton("rectangle"));
+            toolbar.add(makeButton("red"));
+            toolbar.add(makeButton("green"));
             toolbar.add(makeButton("ellipse"));
-            toolbar.add(makeButton("polygon"));
+            toolbar.add(makeButton("no colour"));
             add(toolbar);
         }
+
+        private JRadioButton makeButton(String text){
+            JRadioButton button = new JRadioButton(text);
+            button.setHorizontalAlignment(SwingConstants.LEFT);
+            button.setVerticalAlignment(SwingConstants.BOTTOM);
+            toolGroup.add(button);
     }
     */
 
     static class DrawingArea extends JPanel {
         private final static int AREA_SIZE = 400;
         private ArrayList<ColoredRectangle> coloredRectangles = new ArrayList<ColoredRectangle>();
-        private ColoredRectangle shape = new ColoredRectangle(Color.BLACK, new Rectangle(), ToolSelect.GetTool());
+        private ColoredRectangle shape = new ColoredRectangle(ToolSelect.GetColor(), new Rectangle(), ToolSelect.GetTool());
 
 
 
@@ -136,7 +224,7 @@ public class GUIMk1 {
 
             Color foreground = g.getColor();
 
-            g.setColor(Color.BLACK);
+            g.setColor(ToolSelect.GetColor());
 
                 for (DrawingArea.ColoredRectangle cr : coloredRectangles) {
                     g.setColor(cr.getForeground());
@@ -154,7 +242,7 @@ public class GUIMk1 {
 
                 if (shape != null) {
                     Graphics2D g2d = (Graphics2D) g;
-                    g2d.setColor(foreground);
+                    g2d.setColor(ToolSelect.GetColor());
                     Rectangle c = shape.shape.getBounds();
                     if (ToolSelect.GetTool() == "rectangle") {
                         g.drawRect(c.x, c.y, c.width, c.height);
@@ -172,6 +260,7 @@ public class GUIMk1 {
         public void addRectangle(Rectangle rectangle, Color color, String type) {
             //  Add the Rectangle to the List so it can be repainted
                 ColoredRectangle cr = new ColoredRectangle(color, rectangle, type);
+                System.out.print(color);
                 coloredRectangles.add(cr);
                 repaint();
 
@@ -187,8 +276,8 @@ public class GUIMk1 {
             public void mousePressed(MouseEvent e) {
                 startPoint = e.getPoint();
 
-                shape = new ColoredRectangle(Color.BLACK, new Rectangle(), ToolSelect.GetTool());
-                if (ToolSelect.GetTool() == "plot"){
+                shape = new ColoredRectangle(ToolSelect.GetColor(), new Rectangle(), ToolSelect.GetTool());
+                if (ToolSelect.GetTool() == "plot" || ToolSelect.GetTool() == "line"){
                     shape.shape.setBounds(e.getX(),e.getY(),1,1);
                 }
             }
@@ -202,7 +291,7 @@ public class GUIMk1 {
                 if(ToolSelect.GetTool() != "plot" || ToolSelect.GetTool() != "line") {
                     shape.shape.setBounds(x, y, width, height);
                 } else if (ToolSelect.GetTool() == "line"){
-                    shape.shape.setBounds(startPoint.x,startPoint.y, e.getX(), e.getY());
+                    shape.shape.setBounds(shape.shape.x,shape.shape.y, e.getXOnScreen(), e.getYOnScreen());
                 }
                 repaint();
             }
@@ -211,7 +300,10 @@ public class GUIMk1 {
 
             public void mouseReleased(MouseEvent e) {
                 if (shape.shape.width != 0 || shape.shape.height != 0) {
-                    addRectangle(shape.shape, e.getComponent().getForeground(), ToolSelect.GetTool());
+                    if(ToolSelect.GetTool() == "line"){
+                        shape.shape.setBounds(shape.shape.x, shape.shape.y, e.getX(),e.getY());
+                    }
+                    addRectangle(shape.shape, ToolSelect.GetColor(), ToolSelect.GetTool());
                 }
 
                 shape = null;
